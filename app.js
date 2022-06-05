fff16cb88358d41cd380d9241bbc69184b359bdd
MatiasLaguna <mlaguna@frd.utn.edu.ar>
2022-06-04 23:51:07 -0300

    add button close

--- app.js
+++ app.js
@@ -2,31 +2,41 @@ const inputName = document.getElementById("inputName");
 const inputCost = document.getElementById("inputCost");
 const list = document.getElementById("list-p");
 let ArrayPeople = [];
-
+let restAcum = 0
 
 function validar() {
     ArrayPeople.push([inputName.value, inputCost.value]);
     let li = document.createElement("li");
     li.classList.add("list-group-item");
-    let Close = document.createElement("button")
-    Close.classList.add("btn-close")
     li.innerText = ArrayPeople.at(-1)[0] + ": " + ArrayPeople.at(-1)[1]
+    li.append(closeButton());
     list.append(li);
 
-    Close.addEventListener('click', deteleItem(this.parentElement), false)
-    li.append(Close);
-    let acum = 0
-    for (const people of ArrayPeople) {
-        acum += people[1] - 0
-        console.log(acum)
-    }
-
-    let total = acum / ArrayPeople.length
-    let div = document.getElementById("total");
-    div.innerText = "A pagar cada uno: " + total
+    totalValue();
 }
 
+function closeButton() {
+    let Close = document.createElement("button");
+    Close.addEventListener('click', function deteleItem() {
+        const str = this.parentElement.textContent;
+        const name = str.slice(0, str.indexOf(':'));
+        ArrayPeople = ArrayPeople.filter(elem => elem[0] !== name)
+        console.log(ArrayPeople)
+        this.parentElement.remove();
+        totalValue();
+    }, false);
+    Close.classList.add("btn-close");
+    return Close;
+}
 
-function deteleItem() {
-    console.log(this)
+function totalValue() {
+    let acum = 0;
+    for (const people of ArrayPeople) {
+        acum += (people[1] - 0) - restAcum;
+        console.log(acum);
+    }
+
+    let total = acum / ArrayPeople.length;
+    let div = document.getElementById("total");
+    div.innerText = "A pagar cada uno: " + total;
 }
