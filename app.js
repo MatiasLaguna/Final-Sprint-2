cd62a36760ef97102ac5dfabd0eb807e2f6729c9
MatiasLaguna <mlaguna@frd.utn.edu.ar>
2022-06-12 16:15:29 -0300

    corrijo errores NaN y agrego total

--- app.js
+++ app.js
@@ -2,41 +2,43 @@ const inputName = document.getElementById("inputName");
 const inputCost = document.getElementById("inputCost");
 const list = document.getElementById("list-p");
 let ArrayPeople = [];
-let restAcum = 0
 
 function validar() {
     ArrayPeople.push([inputName.value, inputCost.value]);
     let li = document.createElement("li");
     li.classList.add("list-group-item");
-    li.innerText = ArrayPeople.at(-1)[0] + ": " + ArrayPeople.at(-1)[1]
+    li.innerText = ArrayPeople.at(-1)[0] + ": $" + ArrayPeople.at(-1)[1];
     li.append(closeButton());
     list.append(li);
-
     totalValue();
 }
 
 function closeButton() {
     let Close = document.createElement("button");
-    Close.addEventListener('click', function deteleItem() {
-        const str = this.parentElement.textContent;
-        const name = str.slice(0, str.indexOf(':'));
-        ArrayPeople = ArrayPeople.filter(elem => elem[0] !== name)
-        console.log(ArrayPeople)
-        this.parentElement.remove();
-        totalValue();
-    }, false);
+    Close.addEventListener("click", deteleItem, false);
     Close.classList.add("btn-close");
     return Close;
+}
+
+function deteleItem() {
+    const str = this.parentElement.textContent;
+    const name = str.slice(0, str.indexOf(":"));
+    ArrayPeople = ArrayPeople.filter((elem) => elem[0] !== name);
+    console.log(ArrayPeople);
+    this.parentElement.remove();
+    totalValue();
 }
 
 function totalValue() {
     let acum = 0;
     for (const people of ArrayPeople) {
-        acum += (people[1] - 0) - restAcum;
+        acum += people[1] - 0;
         console.log(acum);
     }
 
-    let total = acum / ArrayPeople.length;
+    let subtotal = acum / ArrayPeople.length;
     let div = document.getElementById("total");
-    div.innerText = "A pagar cada uno: " + total;
+    let divSubtotal = document.getElementById("subtotal");
+    divSubtotal.innerText = "A pagar cada uno: $" + (subtotal ? subtotal : 0);
+    div.innerText = "Total: $" + (acum ? acum : 0);
 }
